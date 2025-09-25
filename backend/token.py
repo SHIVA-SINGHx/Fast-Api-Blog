e0cf8bb307acb5040389f032ace68c7534685521
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-09-25 12:10:32 -0700

    added encoded_jwt in token.py

--- backend/token.py
+++ backend/token.py
@@ -10,3 +10,9 @@ def create_token(data:dict, expires_delta: Optional[timedelta]=None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
+        
+    else:
+        expires_delta = datetime.utcnow() + expires_delta(ACCESS_TOKEN_EXPIRE_MIN)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHAM)
+        return encoded_jwt
